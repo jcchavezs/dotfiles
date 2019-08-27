c270b2e60129a7858fa091b2567ce77225af7225
José Carlos Chávez <jcchavezs@gmail.com>
2019-08-27 15:50:21 +0200

    chore: removes unused installs and adds node.

---
+++ README.md
@@ -0,0 +1,16 @@
+# dotfiles
+
+## Prerequisites
+
+- [SSH key added into github account](https://help.github.com/en/articles/adding-a-new-ssh-key-to-your-github-account)
+- git
+- iterm
+- vscode
+
+## Install
+
+```
+cd ~
+git clone https://github.com/jcchavezs/dotfiles .dotfiles
+./.dotfiles/install.sh
+```
--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-cd ~
+HOME_DIR=`cd ~; pwd`
 
-mkdir ~/Workspace
-mkdir ~/Workspace/source
-mkdir ~/Workspace/gource
-mkdir ~/Workspace/tools
+mkdir ${HOME_DIR}/Workspace
+mkdir ${HOME_DIR}/Workspace/source
+mkdir ${HOME_DIR}/Workspace/gource
+mkdir ${HOME_DIR}/Workspace/tools
 
 # Install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -14,18 +14,16 @@ brew cleanup
 # Setup GIT
 #git config --global user.name "José Carlos Chávez"
 #git config --global user.email jcchavezs@gmail.com
-#git config --global core.excludesfile '~/.dotfiles/.gitignore.global'
+#git config --global core.excludesfile '${HOME_DIR}/.dotfiles/.gitignore.global'
 #git config --global core.editor "/usr/bin/vim"
-ln -sf .dotfiles/.gitconfig ~/.gitconfig
-brew install tig
+ln -sf ${HOME_DIR}/.dotfiles/.gitconfig ${HOME_DIR}/.gitconfig
 brew install pre-commit
-brew install diff-so-fancy
 
 # Install GOLANG
 brew install go
-mkdir ~/Workspace/gource/src
-mkdir ~/Workspace/gource/pkg
-mkdir ~/Workspace/gource/bin
+mkdir -p ${HOME_DIR}/Workspace/gource/src/github.com/jcchavezs
+mkdir ${HOME_DIR}/Workspace/gource/pkg
+mkdir ${HOME_DIR}/Workspace/gource/bin
 brew install grc
 
 # Install PHP
@@ -41,25 +39,21 @@ composer global require symfony/thanks
 
 # Install RVM
 curl -sSL https://get.rvm.io | bash
-ln -s ~/.rvm/scripts/functions/version ~/.rvm/scripts/version
+ln -s ${HOME_DIR}/.rvm/scripts/functions/version ${HOME_DIR}/.rvm/scripts/version
+
+# Install node
+brew install node
+brew install yarn
 
 # Install ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-${HOME_DIR}/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 
 # Install VIM
 brew install vim
 brew install fzf
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-# Install Python
-brew install python
-
-# Install Vagrant
-brew cask install virtualbox
-brew cask install vagrant
-brew cask install vagrant-manager
+git clone https://github.com/VundleVim/Vundle.vim.git ${HOME_DIR}/.vim/bundle/Vundle.vim
 
 # Install httpie
 brew install httpie
@@ -68,8 +62,7 @@ brew install jq
 # Install AG
 brew install the_silver_searcher
 
-mv .zshrc .zshrc_backup && ln -s .dotfiles/.zshrc .zshrc
-mv .vimrc .vimrc_backup && ln -s .dotfiles/.vimrc .vimrc
-ln -s ~/Dropbox/Resources/Settings/.env .env
-ln -s .dotfiles/.grc .grc
-cp ~/.dotfiles/vs-settings.json ~/Library/Application\ Support/Code/User/settings.json
+mv ${HOME_DIR}/.zshrc ${HOME_DIR}/.zshrc_backup && ln -s ${HOME_DIR}/.dotfiles/.zshrc ${HOME_DIR}/.zshrc
+mv (${HOME_DIR}/.vimrc ${HOME_DIR}/.vimrc_backup || true) && ln -s ${HOME_DIR}/.dotfiles/.vimrc ${HOME_DIR}/.vimrc
+ln -s ${HOME_DIR}/.dotfiles/.grc ${HOME_DIR}/.grc
+cp ${HOME_DIR}/.dotfiles/vs-settings.json ${HOME_DIR}/Library/Application\ Support/Code/User/settings.json
