3e40bf8e5df096a8a1013295d7f7628fab3fa146
José Carlos Chávez <jcchavezs@gmail.com>
2019-08-28 15:56:26 +0200

    chore: improves vs-settings.json.

--- install.sh
+++ install.sh
@@ -13,17 +13,39 @@ brew tap caskroom/cask
 ln -sf ${HOME}/.dotfiles/.gitconfig ${HOME}/.gitconfig
 brew install pre-commit
 
+# Install ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+
+# Install VIM
+brew install vim
+brew install fzf
+git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
+
+# Install httpie
+brew install httpie
+
+# Install AG
+brew install the_silver_searcher
+
+mv (${HOME}/.zshrc ${HOME}/.zshrc_backup || true) && ln -s ${HOME}/.dotfiles/.zshrc ${HOME}/.zshrc
+mv (${HOME}/.vimrc ${HOME}/.vimrc_backup || true) && ln -s ${HOME}/.dotfiles/.vimrc ${HOME}/.vimrc
+ln -s ${HOME}/.dotfiles/.grc ${HOME}/.grc
+cp ${HOME}/.dotfiles/vs-settings.json ${HOME}/Library/Application\ Support/Code/User/settings.json
+
 # Install GOLANG
 brew install go
 mkdir -p ${HOME}/Workspace/gource/src/github.com/jcchavezs
 mkdir ${HOME}/Workspace/gource/pkg
 mkdir ${HOME}/Workspace/gource/bin
 brew install grc
+go get golang.org/x/tools/cmd/goimports
 
 # Install PHP
 brew install php71
 
-#Install Composer
+# Install Composer
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 composer global require symfony/thanks
