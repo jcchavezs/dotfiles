65c48c834cc1b921a7109c08f86c7500a9bb4f95
José Carlos Chávez <jcchavezs@gmail.com>
2019-11-26 09:23:11 +0100

    feat: adds command to ammend last commit including existing files.

--- functions.zsh
+++ functions.zsh
@@ -1,8 +1,21 @@
 # Open the Pull Request URL for your current directory's branch (base branch defaults to master)
-function open_pr() {
+function git_open_pr() {
   github_url=`git remote -v | awk '/fetch/{print $2}' | sed -Ee 's#(git@|git://)#https://#' -e 's@com:@com/@' -e 's@biz:@biz/@' -e 's%\.git$%%' | awk '/github/'`;
   branch_name=`git symbolic-ref HEAD | cut -d"/" -f 3,4`;
   pr_url=$github_url"/compare/master..."$branch_name
   echo "Creating a PR for '${branch_name}' via ${pr_url}"
   open $pr_url;
+}
+
+# Undoes the last commit, includes the staged files and redoes the commit.
+function git_ammend_last_commit() {
+  message=`git log -1 --pretty=%B`
+  git --no-pager diff --name-only --staged --exit-code
+  if [[ $? == "0" ]]; then
+    # if there are not staged changes we can't continue
+    echo "Stage your new changes first!"
+    return 1
+  fi
+  git reset HEAD~1 --soft
+  git commit -m "$message"
 }
