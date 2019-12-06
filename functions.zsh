90bfdc54bf639aa3811134f51c962430379a5a9d
José Carlos Chávez <jcchavezs@gmail.com>
2019-12-06 14:24:29 +0000

    feat(git): tweaks git modiffied files to be able to filter by extension.

--- functions.zsh
+++ functions.zsh
@@ -2,6 +2,10 @@
 function git_open_pr() {
   github_url=`git remote -v | awk '/fetch/{print $2}' | sed -Ee 's#(git@|git://)#https://#' -e 's@com:@com/@' -e 's@biz:@biz/@' -e 's%\.git$%%' | awk '/github/'`;
   branch_name=`git symbolic-ref HEAD | cut -d"/" -f 3,4`;
+  if [[ $branch_name == "master" ]]; then
+    echo "It is not possible to open a PR from master. Switch to another branch."
+    return 1
+  fi
   pr_url=$github_url"/compare/master..."$branch_name
   echo "Creating a PR for '${branch_name}' via ${pr_url}"
   open $pr_url;
@@ -18,4 +22,12 @@ function git_ammend_last_commit() {
   fi
   git reset HEAD~1 --soft
   git commit -m "$message"
+}
+
+function git_modified_files() {
+  if [[ $1 == "" ]]; then
+    git status -s | awk '{if ($1 == "M") print $2}'
+  else
+    git status -s | awk '{if ($1 == "M") print $2}' | grep -n "$1" | awk -F: '{print $2}'
+  fi  
 }
