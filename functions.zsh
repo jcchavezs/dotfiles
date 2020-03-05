aa378262331e3906feb1eefebef96b0fc1fa4653
José Carlos Chávez <jcchavezs@gmail.com>
2020-03-05 09:25:03 +0100

    chore: removes gpr as git provides that functionality already.

--- functions.zsh
+++ functions.zsh
@@ -1,16 +1,3 @@
-# Open the Pull Request URL for your current directory's branch (base branch defaults to master)
-function git_open_pr() {
-  github_url=`git remote -v | awk '/fetch/{print $2}' | sed -Ee 's#(git@|git://)#https://#' -e 's@com:@com/@' -e 's@biz:@biz/@' -e 's%\.git$%%' | awk '/github/'`;
-  branch_name=`git symbolic-ref HEAD | cut -d"/" -f 3,4`;
-  if [[ $branch_name == "master" ]]; then
-    echo "It is not possible to open a PR from master. Switch to another branch."
-    return 1
-  fi
-  pr_url=$github_url"/compare/master..."$branch_name
-  echo "Creating a PR for '${branch_name}' via ${pr_url}"
-  open $pr_url;
-}
-
 # Undoes the last commit, includes the staged files and redoes the commit.
 function git_ammend_last_commit() {
   message=`git log -1 --pretty=%B`
@@ -24,14 +11,30 @@ function git_ammend_last_commit() {
   git commit -m "$message"
 }
 
-# List all added/modified files that are not deleted.
+# List all added/modified files that are not deleted matching a regex name and a word
+# Usage: gmf [filename_pattern [search_word]]
+# E.g. : gmf '.kt' avro
 function git_modified_files() {
-  if [[ $1 == "" ]]; then
-    # list files | filter in those that are added or modified but not deleted
-    git status -s -u | awk '{if ((($1 ~ "M" || $1 ~ "A") && $1 !~ "D") || $1 == "??") print $2}'
+  if [[ $# -gt 2 ]]; then
+    echo "Too many arguments, did you pass .* as first argument? pass '.*' instead."
+    return 1
+  fi
+  if [[ $1 == "" || $1 == ".*" ]]; then
+    if [[ $2 == "" ]]; then
+      # list files | filter in those that are added or modified but not deleted
+      git status -s -u | awk '{if ((($1 ~ "M" || $1 ~ "A") && $1 !~ "D") || $1 == "??") print $2}'
+    else
+      # list files | filter in those that are added or modified but not deleted | filter by extension
+      git status -s -u | awk '{if ((($1 ~ "M" || $1 ~ "A") && $1 !~ "D") || $1 == "??") print $2}' | xargs grep -l -w "$2"
+    fi
   else
-    # list files | filter in those that are added or modified but not deleted | filter by extension | remove the bullets
-    git status -s -u | awk '{if ((($1 ~ "M" || $1 ~ "A") && $1 !~ "D") || $1 == "??") print $2}' | grep -n "$1" | awk -F: '{print $2}'
+    if [[ $2 == "" ]]; then
+      # list files | filter in those that are added or modified but not deleted | filter by extension | remove the bullets
+      git status -s -u | awk '{if ((($1 ~ "M" || $1 ~ "A") && $1 !~ "D") || $1 == "??") print $2}' | grep -n "$1" | awk -F: '{print $2}'
+    else
+      # list files | filter in those that are added or modified but not deleted | filter by extension | remove the bullets | filter by text 
+      git status -s -u | awk '{if ((($1 ~ "M" || $1 ~ "A") && $1 !~ "D") || $1 == "??") print $2}' | grep -n "$1" | awk -F: '{print $2}' | xargs grep -l -w "$2"
+    fi
   fi  
 }
 
