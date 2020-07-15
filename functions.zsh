e1c59ee59e31014a650f239c4526c467d60e74cc
José Carlos Chávez <jcchavezs@gmail.com>
2020-07-15 22:59:21 +0200

    feat(k8s): adds commands and functions.

--- functions.zsh
+++ functions.zsh
@@ -47,13 +47,125 @@ function git_modified_files() {
   fi  
 }
 
-# Creates a wip branch (if not in one already) and a wip commit with all modified/added files.
-function git_wip() {
-  branch_name=`git symbolic-ref HEAD | cut -d"/" -f 3,4`;
-  if [[ ! $branch_name == wip* ]]; then
-    gco -b wip_$(date +"%Y-%m-%d")
+# Creates a branch and adds a wip commit with all modified/added files.
+function git_branch_wip() {
+  if [[ "$1" == "" ]]; then
+    echo "First argument (branch name) is required."
+    return 1
   fi
+
+  current_branch=`git symbolic-ref HEAD | cut -d"/" -f 3,4`;
+  gco -b $1
   git add .
   git commit -m "wip" --no-verify
-  gco master
+  gco $current_branch
+}
+
+function kube_use_namespace() {
+  kubectl config set-context --current --namespace=$1
+  current_namespace=$(kube_get_current_namespace)
+  echo "Current namespace: $current_namespace"
+}
+
+function kube_get_current_namespace() {
+  kubectl config view --minify | grep namespace: | awk '{print $2}'
+}
+
+function kube_reset_namespace() {
+  kubectl config set-context --current --namespace=kube-system
+}
+
+function kube_list_pods() {
+  NAMESPACE_ARGS=""
+  if [[ ! "$1" == "" ]]; then
+    NAMESPACE_ARGS="-n $1"
+  fi
+
+  CMD="kubectl get pods $NAMESPACE_ARGS"
+  sh -c "$CMD"
+}
+
+# list the running pods for a given app (optionally in a namespace)
+function kube_get_pods() {
+  NAMESPACE_ARGS=""
+  if [[ "$#" == "2" ]]; then
+    NAMESPACE_ARGS="-n $2"
+  fi
+
+  CMD="kubectl get pods $NAMESPACE_ARGS"
+  if [[ ! "$1" == "" ]]; then
+    CMD="$CMD --selector=app=$1"
+  fi
+  sh -c "$CMD"
+}
+
+# list the running pod images for a given app (optionally in a namespace)
+function kube_get_app_images() {
+  if [[ "$1" == "" ]]; then
+    echo "First agument (app label) is required."
+    return 1
+  fi
+
+  NAMESPACE_ARGS=""
+  if [[ "$#" == "2" ]]; then
+    NAMESPACE_ARGS="-n $2"
+  fi
+
+  sh -c "kubectl get pod -o json --selector=app=$1 $NAMESPACE_ARGS"  | jq '.items' | jq -c '.[].spec.containers' | jq -r -c '.[].image' | grep $1
+}
+
+function kube_get_pod_image() {
+  if [[ "$1" == "" ]]; then
+    echo "First agument (pod name) is required."
+    return 1
+  fi
+
+  NAMESPACE_ARGS=""
+  if [[ "$#" == "2" ]]; then
+    NAMESPACE_ARGS="-n $2"
+  fi
+
+  sh -c "kubectl get pod -o json $1 $NAMESPACE_ARGS"  | jq -c '.spec.containers' | jq -r -c '.[].image'
+}
+
+function kube_describe_pod() {
+  NAMESPACE_ARGS=""
+  if [[ "$#" == "2" ]]; then
+    NAMESPACE_ARGS="-n $2"
+  fi
+
+  sh -c "kubectl describe pod $1 $NAMESPACE_ARGS"
+}
+
+function kube_logs() {
+  CMD="kubectl logs"
+  if [[ "$1" == "-f" ]]; then
+    CMD="$CMD -f"
+    shift
+  fi
+
+  if [[ "$1" == "" ]]; then
+    echo "First agument (pod name) is required."
+    return 1
+  fi
+
+  NAMESPACE_ARGS=""
+  if [[ "$#" == "2" ]]; then
+    NAMESPACE_ARGS="-n $2"
+  fi
+
+  CONTAINER_ARGS=""
+  if [[ "$#" == "3" ]]; then
+    CONTAINER_ARGS="-c $3"
+  fi
+
+  sh -c "$CMD $1 $NAMESPACE_ARGS $CONTAINER_ARGS"
+}
+
+function kube_exec() {
+  CMD="/bin/sh"
+  if [[ ! "$2" == "" ]]; then
+     CMD="$2"
+  fi
+  kubectl exec -it $1 -- $CMD
 }
