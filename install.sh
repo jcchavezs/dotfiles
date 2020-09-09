7b67e2707605925e560fbf9fe853f50c51fd4736
José Carlos Chávez <jcchavezs@gmail.com>
2020-09-09 10:48:34 +0200

    chore: several fixes for a new install. (#2)

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-mkdir -p ${HOME}/Workspace/source/github.com/jcchavezs || true
-mkdir ${HOME}/Workspace/gource || true
-mkdir ${HOME}/Workspace/tools || true
+touch ${HOME}/.env
+
+mkdir -p ${HOME}/Workspace/source/github.com/jcchavezs
+mkdir ${HOME}/Workspace/gource
+mkdir ${HOME}/Workspace/tools
 
 # Install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew update
 brew upgrade
 brew cleanup
-brew tap homebrew/cask-cask
 
 # Setup GIT
 ln -sf ${HOME}/.dotfiles/.gitconfig ${HOME}/.gitconfig
@@ -17,12 +18,15 @@ brew install pre-commit
 
 # Install ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+rm -rf $ZSH_CUSTOM/plugins/zsh-autosuggestions || true
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+rm -rf ${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins/zsh-history-substring-search || true
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 
 # Install VIM
 brew install vim
 brew install fzf
+rm -rf ${HOME}/.vim/bundle/Vundle.vim || true
 git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
 
 # Install mysql-client
@@ -37,21 +41,26 @@ brew install htop
 # Install AG
 brew install the_silver_searcher
 
-mv (${HOME}/.zshrc ${HOME}/.zshrc_backup || true) && ln -s ${HOME}/.dotfiles/.zshrc ${HOME}/.zshrc
+(mv ${HOME}/.zshrc ${HOME}/.zshrc_backup || true) && ln -s ${HOME}/.dotfiles/.zshrc ${HOME}/.zshrc
 mkdir -p ${ZSH_CUSTOM}/custom
 ln -s ${HOME}/.dotfiles/functions.zsh ${ZSH_CUSTOM}/functions.zsh
 
-mv (${HOME}/.vimrc ${HOME}/.vimrc_backup || true) && ln -s ${HOME}/.dotfiles/.vimrc ${HOME}/.vimrc
+(mv ${HOME}/.vimrc ${HOME}/.vimrc_backup || true) && ln -s ${HOME}/.dotfiles/.vimrc ${HOME}/.vimrc
 ln -s ${HOME}/.dotfiles/.grc ${HOME}/.grc
 cp ${HOME}/.dotfiles/vs-settings.json ${HOME}/Library/Application\ Support/Code/User/settings.json
 
 # Install Go
 brew install go
-mkdir -p ${HOME}/Workspace/gource/src/github.com/jcchavezs
 mkdir ${HOME}/Workspace/gource/pkg
 mkdir ${HOME}/Workspace/gource/bin
 brew install grc
 go get golang.org/x/tools/cmd/goimports
+
+# Install protobuf
+brew install protobuf
+export GO111MODULE=on
+go get github.com/golang/protobuf/protoc-gen-go
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc
 
 # Install PHP
 brew install php@7.3
@@ -89,12 +98,15 @@ brew install clang-format
 
 # Install ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+rm -rf $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+rm -rf ${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 
 # Install VIM
 brew install vim
 brew install fzf
+rm -rf ${HOME}/.vim/bundle/Vundle.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
 
 # Install httpie
@@ -111,8 +123,8 @@ brew install retry
 # Install AG
 brew install the_silver_searcher
 
-mv (${HOME}/.zshrc ${HOME}/.zshrc_backup || true) && ln -s ${HOME}/.dotfiles/.zshrc ${HOME}/.zshrc
-mv (${HOME}/.vimrc ${HOME}/.vimrc_backup || true) && ln -s ${HOME}/.dotfiles/.vimrc ${HOME}/.vimrc
+(mv ${HOME}/.zshrc ${HOME}/.zshrc_backup || true) && ln -s ${HOME}/.dotfiles/.zshrc ${HOME}/.zshrc
+(mv ${HOME}/.vimrc ${HOME}/.vimrc_backup || true) && ln -s ${HOME}/.dotfiles/.vimrc ${HOME}/.vimrc
 ln -s ${HOME}/.dotfiles/.grc ${HOME}/.grc
 
 # Install latex
@@ -123,7 +135,7 @@ sudo tlmgr install latexmk enumitem stmaryrd collection-fontsrecommended
 # Add VS Code settings
 cp ${HOME}/.dotfiles/vs-settings.json ${HOME}/Library/Application\ Support/Code/User/settings.json
 
-extensions='./vs-extensions.txt'
+extensions="${HOME}/.dotfiles/vs-extensions.txt"
 while IFS= read -r line
 do
   code --install-extension $line
